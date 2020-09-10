eba404cee4d8252567c9425e0fce9054a3186ab1
wenziyang <1483319587@qq.com>
2020-09-10 23:03:02 +0800

    add 每日一题：leetcode 1572题，矩阵对角线求和

---
+++ daily_question/python/diagonal_sum.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+from typing import List
+
+# 题目说明：详细题解或者说明参见leetCode 1572.矩阵对角线元素的和
+# 矩阵(二维数组)对角线的和，
+'''
+mat = [[1,2,3],
+       [4,5,6],
+       [7,8,9]
+      ]
+      
+sum = 1 + 5 + 9 + 3 + 7   
+
+提示：
+n == mat.length == mat[i].length
+1 <= n <= 100
+1 <= mat[i][j] <= 100   
+'''
+
+
+def diagonal_sum(mat: List[List[int]]) -> int:
+    print(mat)
+    return 10
