5890f5ef9aff273e4ec3c4f9f3b3c42246d45c72
wenziyang <wenziyangday@gmail.com>
2020-09-15 17:16:58 +0800

    update 每日一题：leetcode 1266题，访问所有点的最小时间 【数理逻辑存疑】

--- daily_question/python/min_time_visit_all_point.py
+++ daily_question/python/min_time_visit_all_point.py
@@ -13,6 +13,18 @@ from typing import List
 '''
 
 
-def min_time_to_visit_all_points(points: List[List[int]]) -> int:
-    print(points)
-    return 10
+def minTimeToVisitAllPoints(points: List[List[int]]) -> int:
+    minTime = 0
+    for i in range(len(points) - 1):
+        disX = abs(points[i][0] - points[i + 1][0])
+        disY = abs(points[i][1] - points[i + 1][1])
+        if disX > disY:
+            minTime += disX
+        else:
+            minTime += disY
+
+    return minTime
+
+
+if __name__ == '__main__':
+    print(minTimeToVisitAllPoints([[1, 1], [3, 4], [-1, 0]]))
