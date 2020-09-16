2a1335f31a9d5851b218a8b6a41010b5dbb67266
wenziyang <wenziyangday@gmail.com>
2020-09-16 11:13:40 +0800

    add 每日一题：leetcode 1351题，统计有序矩阵中的负数

---
+++ daily_question/python/count_negatives.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+from typing import List
+
+# 1351. 统计有序矩阵中的负数
+
+"""
+给你一个 m * n 的矩阵 grid，矩阵中的元素无论是按行还是按列，都以非递增顺序排列。
+
+请你统计并返回 grid 中 负数 的数目。
+"""
+
+
+def countNegatives(grid: List[List[int]]) -> int:
+    print(grid)
+    pass
+
+
+if __name__ == '__main__':
+    pass
