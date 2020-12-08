b3e53354b0f35c4785803c1d19fd4e6be52b0da1
admin <Vincent.wen@wayz.ai>
2020-12-08 18:29:01 +0800

    add 每日一题：LeetCode 1252题，奇数值单元格的数组

---
+++ js/leetCode/oddCells.js
@@ -0,0 +1,19 @@
+/**
+ * @description LeetCode 1252题 奇数值单元格的数组 n行数 m列数 初始化值是一个 n 行 m 列 的 值为0的 二维数组 indices 是一个坐标数组
+ * @param {number} n
+ * @param {number} m
+ * @param {number[][]} indices
+ * @return {number}
+ * */
+
+
+// 生成一个n、m 的矩阵(n行 m列的二维数组) 并且数组中的每个元素的初始值 为0
+
+function oddCells(n, m, indices) {
+
+  let nms = new Array(n).fill(new Array(m).fill(0));
+
+  return 0;
+}
+
+oddCells(2, 3, [[0, 1]]);
