a65437ecdd0902889fa04afa64a50a4132817b85
vicent.wen <Vincent.wen@wayz.ai>
2021-05-14 18:17:29 +0800

    add 基础知识点-- 链表、浏览器

--- js/概念/验证/proto.js
+++ js/概念/验证/proto.js
@@ -3,16 +3,6 @@ function Person() {
 }
 console.log(Person.prototype);
 const p = new Person();
-// console.log(p.constructor, p.__proto__, Person.prototype);
-//
-// console.log(p.constructor === Person);
-// console.log(p.__proto__ === Person.prototype, 'l;');
-// console.log(p.__proto__.constructor === Person);
-// console.log(Person.prototype.constructor === Person);
-//
-//
-// console.log(Person.prototype, Person.constructor);
-
 console.log(Person.prototype.constructor === Person);
 console.log(p.constructor === Person);
 console.log(Person.constructor);
